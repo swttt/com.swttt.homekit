a31389570abe64ab9cdd07a0440f8d4d0883ec94
Bas Jansen <b@sjansen.email>
2017-07-11 22:20:35 +0200

    Test

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
 "use strict";
 
+const HAS = require('has-node')
 
 function init() {
 
   Homey.log("Starting homekit!");
 
-const HAS = require('has-node')
- 
-let config = new HAS.Config('NodeJS Fan', '82:E6:B6:63:BC:2C', HAS.categories.fan, __dirname + 'userdata/fan.json', 8091, '200-20-200');
- 
-let server = new HAS.Server(config);
- 
-//Fan 
-let fan = new HAS.Accessory(1);
- 
-let fanIdentify = HAS.predefined.Identify(1, undefined, (value, callback) => {
-        console.log('Fan Identify', value);
-        callback(HAS.statusCodes.OK);
+
+
+  let config = new HAS.Config('Homey', '81:E6:B6:43:BC:2C', HAS.categories.bridge, __dirname + '/userdata/homey.json', 8090, '123-00-123');
+
+  let server = new HAS.Server(config);
+
+  //light
+  let bridge = new HAS.Accessory(1);
+
+  let identify = HAS.predefined.Identify(1, undefined, (value, callback) => {
+      console.log('Bridge Identify', value);
+      callback(HAS.statusCodes.OK);
     }),
-    fanManufacturer = HAS.predefined.Manufacturer(2, 'Hamyar'),
-    fanModel = HAS.predefined.Model(3, 'Model2017'),
-    fanName = HAS.predefined.Name(4, 'Fan'),
-    fanSerialNumber = HAS.predefined.SerialNumber(5, 'ABCDEFGHIJ'),
-    fanFirmwareVersion = HAS.predefined.FirmwareRevision(6, '1.0.0');
-fan.addServices(HAS.predefined.AccessoryInformation(1, [fanIdentify, fanManufacturer, fanModel, fanName, fanSerialNumber, fanFirmwareVersion]));
- 
- 
-let on = HAS.predefined.On(1, false, (value, callback) => {
-    console.log('Fan Status', value);
+    manufacturer = HAS.predefined.Manufacturer(2, 'Hamyar'),
+    model = HAS.predefined.Model(3, 'Model2017'),
+    name = HAS.predefined.Name(4, 'Bridge'),
+    serialNumber = HAS.predefined.SerialNumber(5, 'ABCDEFGH'),
+    firmwareVersion = HAS.predefined.FirmwareRevision(6, '1.0.0');
+  bridge.addServices(HAS.predefined.AccessoryInformation(1, [identify, manufacturer, model, name, serialNumber, firmwareVersion]));
+
+  server.addAccessory(bridge);
+
+
+  //light
+  let light = new HAS.Accessory(2);
+
+  let lightIdentify = HAS.predefined.Identify(1, undefined, (value, callback) => {
+      console.log('Light Identify', value);
+      callback(HAS.statusCodes.OK);
+    }),
+    lightManufacturer = HAS.predefined.Manufacturer(2, 'Fibaro'),
+    lightModel = HAS.predefined.Model(3, 'Dimmer2'),
+    lightName = HAS.predefined.Name(4, 'Dimmer'),
+    lightSerialNumber = HAS.predefined.SerialNumber(5, 'ABCDEFGHIJ'),
+    lightFirmwareVersion = HAS.predefined.FirmwareRevision(6, '1.0.0');
+  light.addServices(HAS.predefined.AccessoryInformation(1, [lightIdentify, lightManufacturer, lightModel, lightName, lightSerialNumber, lightFirmwareVersion]));
+
+
+  let on = HAS.predefined.On(1, false, (value, callback) => {
+    console.log('Light Status', value);
     callback(HAS.statusCodes.OK);
-});
-fan.addServices(HAS.predefined.Fan(2, [on]));
- 
-server.addAccessory(fan);
- 
-//server.onIdentify will be used only when server is not paired, If server is paired identify.onWrite will be used 
-server.onIdentify = fanIdentify.onWrite;
- 
-//Starts the server 
-server.startServer();
+  });
+  light.addServices(HAS.predefined.Lightbulb(2, [on]));
+  //server.onIdentify will be used only when server is not paired, If server is paired identify.onWrite will be used
+  server.onIdentify = identify.onWrite;
+
+  server.addAccessory(light);
+
+  //Starts the server
+  server.startServer();
 
 }
 
