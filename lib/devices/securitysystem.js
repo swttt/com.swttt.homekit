swttt/com.swttt.homekit

commit 093fd7bf5ffdd73231a16dbba7e377f1c798c37f

Support `onoff` for class `homealarm` (#129)

lib/devices/securitysystem.js
@@ -38,13 +38,11 @@ function value2state(value) {
 }
 
 module.exports = function(device, api, capabilities) {
+  // Init device
+  let homekitAccessory = new Accessory(device.name, device.id);
 
   // Var that keeps the target state
-  var targetStateValue = null;
-
-  // Init device
-  var homekitAccessory = new Accessory(device.name, device.id);
-
+  let targetStateValue = null;
 
   // Set device info
   homekitAccessory
@@ -65,43 +63,57 @@ module.exports = function(device, api, capabilities) {
     .addService(Service.SecuritySystem, device.name)
     .getCharacteristic(Characteristic.SecuritySystemCurrentState)
     .on('get', function(callback) {
-      //support for Heimdall app alarm capability
-      if ('alarm_heimdall' in device.capabilitiesObj && device.capabilitiesObj.alarm_heimdall.value === true){
-        callback(null, 4);
-      } else {
+      // Support for Heimdall app alarm capability
+      if ('alarm_heimdall' in device.capabilitiesObj && device.capabilitiesObj.alarm_heimdall.value === true) {
+        return callback(null, 4);
+      } else if ('homealarm_state' in device.capabilities) {
         const value = getCapabilityValue(device, 'homealarm_state');
-        callback(null, value ? state2value(value, targetStateValue) : this.value);
+        return callback(null, value ? state2value(value, targetStateValue) : this.value);
+      } else if ('onoff' in device.capabilities) { // Satel Integra
+        const value = getCapabilityValue(device, 'onoff');
+        return callback(null, value ? state2value(value ? 'armed' : 'disarmed', targetStateValue) : this.value);
       }
+      return callback(Error('NO_CAPABILITY'));
     });
 
-  // Target capabilitiesObj
+  // Target state
   homekitAccessory
     .getService(Service.SecuritySystem)
     .getCharacteristic(Characteristic.SecuritySystemTargetState)
     .on('set', function(value, callback) {
-    targetStateValue = value;
-      device.setCapabilityValue('homealarm_state', value2state(value) )
-      callback();
+      targetStateValue = value;
+      if ('homealarm_state' in device.capabilities) {
+        device.setCapabilityValue('homealarm_state', value2state(value) )
+      } else if ('onoff' in device.capabilities) {
+        device.setCapabilityValue('onoff', value ? 'armed' : 'disarmed');
+      }
+      return callback();
     })
     .on('get', function(callback) {
-      const value = getCapabilityValue(device, 'homealarm_state');
-      callback(null, value ? state2value(value, targetStateValue) : this.value);
+      if ('homealarm_state' in device.capabilities) {
+        const value = getCapabilityValue(device, 'homealarm_state');
+        return callback(null, value ? state2value(value, targetStateValue) : this.value);
+      } else if ('onoff' in device.capabilities) { // Satel Integra
+        const value = getCapabilityValue(device, 'onoff');
+        return callback(null, value ? state2value(value ? 'armed' : 'disarmed', targetStateValue) : this.value);
+      }
+      return callback(Error('NO_CAPABILITY'));
     });
 
   // Tamper
-  if('alarm_tamper' in device.capabilities){
-  homekitAccessory
-    .getService(Service.SecuritySystem)
-    .getCharacteristic(Characteristic.StatusTampered)
-    .on('get', function(callback) {
-      const value = getCapabilityValue(device, 'alarm_tamper');
-      callback(null, value ? ~~value : this.value);
-    });
+  if ('alarm_tamper' in device.capabilities) {
+    homekitAccessory
+      .getService(Service.SecuritySystem)
+      .getCharacteristic(Characteristic.StatusTampered)
+      .on('get', function(callback) {
+        const value = getCapabilityValue(device, 'alarm_tamper');
+        callback(null, value ? ~~value : this.value);
+      });
   }
 
   // On realtime event update the device
   for (let i in device.capabilities) {
-    if (['alarm_heimdall','homealarm_state','alarm_tamper'].includes(device.capabilities[i].split('.')[0])) {
+    if ([ 'alarm_heimdall', 'homealarm_state', 'alarm_tamper', 'onoff' ].includes(device.capabilities[i].split('.')[0])) {
       console.log('created listener for - ' + device.capabilities[i]);
       let listener = async (value) => {
         onStateChange(device.capabilities[i], value, device);
@@ -112,31 +124,29 @@ module.exports = function(device, api, capabilities) {
   }
 
   async function onStateChange(capability, value, device) {
-
     console.log('State Change - ' + device.name + ' - ' + capability + ' - ' + value);
+    const securitysystem = homekitAccessory.getService(Service.SecuritySystem);
 
-  const securitysystem = homekitAccessory.getService(Service.SecuritySystem);
-
-  //support for Heimdall app alarm capability
-  if (capability == 'alarm_heimdall' && value == true){
+    // Support for Heimdall app alarm capability
+    if (capability == 'alarm_heimdall' && value == true) {
       securitysystem.getCharacteristic(Characteristic.SecuritySystemCurrentState)
         .updateValue(4);
-  }
-
-  else if (capability == 'homealarm_state') {
+    } else if (capability == 'homealarm_state') {
       securitysystem.getCharacteristic(Characteristic.SecuritySystemTargetState)
-      .updateValue(state2value(value, targetStateValue));
-
-    securitysystem.getCharacteristic(Characteristic.SecuritySystemCurrentState)
-      .updateValue(state2value(value, targetStateValue));
-  }
-
-    else if (capability == 'alarm_tamper'){
+        .updateValue(state2value(value, targetStateValue));
+      securitysystem.getCharacteristic(Characteristic.SecuritySystemCurrentState)
+        .updateValue(state2value(value, targetStateValue));
+    } else if (capability === 'onoff') {
+      securitysystem.getCharacteristic(Characteristic.SecuritySystemTargetState)
+        .updateValue(state2value(value ? 'armed' : 'disarmed', targetStateValue));
+      securitysystem.getCharacteristic(Characteristic.SecuritySystemCurrentState)
+        .updateValue(state2value(value ? 'armed' : 'disarmed', targetStateValue));
+    } else if (capability === 'alarm_tamper') {
       securitysystem.getCharacteristic(Characteristic.StatusTampered)
         .updateValue(~~value);
     }
   }
 
   // Return device to app.js
-  return homekitAccessory
+  return homekitAccessory;
 }

app.js
@@ -186,7 +186,7 @@ class HomekitApp extends Homey.App {
       this.log('Found doorbell: ' + device.name)
       isPaired = true;
       bridge.addBridgedAccessory(homekit.createDoorbell(device, api));
-    } else if ('homealarm_state' in capabilities) {
+    } else if ('homealarm_state' in capabilities || device.class === 'homealarm' || device.virtualClass === 'homealarm') {
       this.log('Found Security system: ' + device.name)
       isPaired = true;
       bridge.addBridgedAccessory(homekit.createSecuritySystem(device, api, capabilities));

commit 86434eb5f8c92a6ca226427e5276a42cc389d969

Fixed some issues introduced by 093fd7bf

lib/devices/securitysystem.js
@@ -66,10 +66,10 @@ module.exports = function(device, api, capabilities) {
       // Support for Heimdall app alarm capability
       if ('alarm_heimdall' in device.capabilitiesObj && device.capabilitiesObj.alarm_heimdall.value === true) {
         return callback(null, 4);
-      } else if ('homealarm_state' in device.capabilities) {
+      } else if ('homealarm_state' in capabilities) {
         const value = getCapabilityValue(device, 'homealarm_state');
         return callback(null, value ? state2value(value, targetStateValue) : this.value);
-      } else if ('onoff' in device.capabilities) { // Satel Integra
+      } else if ('onoff' in capabilities) { // Satel Integra
         const value = getCapabilityValue(device, 'onoff');
         return callback(null, value ? state2value(value ? 'armed' : 'disarmed', targetStateValue) : this.value);
       }
@@ -82,18 +82,18 @@ module.exports = function(device, api, capabilities) {
     .getCharacteristic(Characteristic.SecuritySystemTargetState)
     .on('set', function(value, callback) {
       targetStateValue = value;
-      if ('homealarm_state' in device.capabilities) {
+      if ('homealarm_state' in capabilities) {
         device.setCapabilityValue('homealarm_state', value2state(value) )
-      } else if ('onoff' in device.capabilities) {
+      } else if ('onoff' in capabilities) {
         device.setCapabilityValue('onoff', value ? 'armed' : 'disarmed');
       }
       return callback();
     })
     .on('get', function(callback) {
-      if ('homealarm_state' in device.capabilities) {
+      if ('homealarm_state' in capabilities) {
         const value = getCapabilityValue(device, 'homealarm_state');
         return callback(null, value ? state2value(value, targetStateValue) : this.value);
-      } else if ('onoff' in device.capabilities) { // Satel Integra
+      } else if ('onoff' in capabilities) { // Satel Integra
         const value = getCapabilityValue(device, 'onoff');
         return callback(null, value ? state2value(value ? 'armed' : 'disarmed', targetStateValue) : this.value);
       }
@@ -101,7 +101,7 @@ module.exports = function(device, api, capabilities) {
     });
 
   // Tamper
-  if ('alarm_tamper' in device.capabilities) {
+  if ('alarm_tamper' in capabilities) {
     homekitAccessory
       .getService(Service.SecuritySystem)
       .getCharacteristic(Characteristic.StatusTampered)